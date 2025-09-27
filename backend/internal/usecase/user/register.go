dc8ed4aba8178e0d3a5fa2fec4e9fb180ccf4956
filinovdg <filinovdg@student.bmstu.ru>
2025-09-27 19:11:17 +0300

    добавил регистрацию с автологином. пофиксил названия пакетов и вынес общие функции в utils.go

---
+++ backend/internal/usecase/user/register.go
@@ -0,0 +1,63 @@
+package register
+
+import (
+	"context"
+	"errors"
+	"github.com/ostrovok-hackathon-2025/afrikanskie-petushki/backend/docs"
+)
+
+// OstrovokUser представляет пользователя из системы Островок
+type OstrovokUser struct {
+	Login     string `json:"login"`
+	Email     string `json:"email"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+// OstrovokService интерфейс для работы с API Островок
+type OstrovokService interface {
+	GetUserByLogin(ctx context.Context, login string) (*OstrovokUser, error)
+}
+
+type RegistrationService struct {
+	userRepo    UserRepository
+	ostrovokSvc OstrovokService
+	jwtSecret   []byte
+}
+
+type RegisterRequest struct {
+	OstrovokLogin string `json:"ostrovok_login"`
+	Password      string `json:"password"`
+}
+
+func (s *RegistrationService) Register(ctx context.Context, req RegisterRequest) (*docs.AuthResponse, error) {
+	// 1. Получаем пользователя из системы Островок
+	ostrovokUser, err := s.ostrovokSvc.GetUserByLogin(ctx, req.OstrovokLogin)
+	if err != nil {
+		return nil, err
+	}
+
+	exists, err := s.userRepo.UserExists(ctx, req.OstrovokLogin)
+	if err != nil {
+		return nil, err
+	}
+	if exists {
+		return nil, errors.New("пользователь уже зарегистрирован")
+	}
+
+	passwordHash := hashPassword(req.Password)
+
+	user := &User{
+		ID:            generateUserID(),
+		OstrovokLogin: ostrovokUser.Login,
+		Email:         ostrovokUser.Email,
+		IsAdmin:       false,
+	}
+
+	err = s.userRepo.CreateUser(ctx, user, passwordHash)
+	if err != nil {
+		return nil, err
+	}
+
+	return generateTokens(user, s.jwtSecret)
+}
