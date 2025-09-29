9cf73260608386e4a3e5fe53c662f77e0dc8aaef
Yaroslav Chicherin <y.chicherin@vk.team>
2025-09-29 23:41:36 +0300

    write choose user by rating

---
+++ backend/pkg/choose_by_rating.go
@@ -0,0 +1,33 @@
+package pkg
+
+import (
+	"math"
+	"math/rand"
+
+	"github.com/google/uuid"
+	model "github.com/ostrovok-hackathon-2025/afrikanskie-petushki/backend/internal/model/user"
+)
+
+func ChooseByRating(users []model.User) uuid.UUID {
+	probabilities := make([]float64, len(users))
+
+	sum := 0.0
+	for i, user := range users {
+		probability := probabilityFromRating(user.Rating)
+		probabilities[i] = probability
+		sum += probability
+	}
+
+	target := rand.Float64() * sum
+	for i, p := range probabilities {
+		target -= p
+		if target < 0 {
+			return users[i].ID
+		}
+	}
+	return uuid.Nil
+}
+
+func probabilityFromRating(rating int) float64 {
+	return math.Sqrt(float64(rating))
+}
